11061b8840b57e34636fa3a288a8f7bffd27afbd
ajiken4610 <ajiken4610@gmail.com>
2023-10-15 02:10:16 +0900

    ライブラリ入れた

---
+++ nuxt.config.ts
@@ -0,0 +1,22 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  typescript: {
+    tsConfig: {
+      extends: "@tsconfig/strictest/tsconfig.json",
+    },
+  },
+  css: ["~/assets/css/main.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  ssr: false,
+  router: {
+    options: {
+      hashMode: true,
+    },
+  },
+});
